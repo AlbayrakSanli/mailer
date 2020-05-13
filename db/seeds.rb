23840475daa687201935e9da45cd9d757ce2bd8d
Albayrak Furkan <albayrak.mo@hotmail.fr>
2020-05-13 17:51:26 +0200

    quatrième commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ require 'faker'
 
 # Génération de 10 users
 10.times do
-  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::TvShows::GameOfThrones.quote, email: Faker::Internet.email(domain: 'yopmail.com'), password: Faker::Lorem.characters(number: 10))
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::TvShows::GameOfThrones.quote, email: Faker::Internet.email(domain: 'yopmail.com'), encrypted_password: Faker::Lorem.characters(number: 10))
 end
